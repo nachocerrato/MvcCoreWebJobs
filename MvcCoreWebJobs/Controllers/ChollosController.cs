759b8b1d45ac8c68e605c2c84126d906b4524ff6
nachocerrato <nacho.cr.94@gmail.com>
2022-01-28 19:22:34 +0100

    Add project files.

---
+++ MvcCoreWebJobs/Controllers/ChollosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreWebJobs.Models;
+using MvcCoreWebJobs.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreWebJobs.Controllers
+{
+    public class ChollosController : Controller
+    {
+        private RepositoryChollos repo;
+        public ChollosController(RepositoryChollos repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(this.repo.GetChollos());
+        }
+    }
+}
---
+++ MvcCoreWebJobs/Data/ChollosContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreWebJobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreWebJobs.Data
+{
+    public class ChollosContext: DbContext
+    {
+        public ChollosContext(DbContextOptions<ChollosContext> options)
+            : base(options) { }
+        public DbSet<Chollo> Chollos { get; set; }
+
+    }
+}
